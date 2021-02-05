aee09560d4a364a8a88328a0c3335efb69cf8d97
Dimitris Halatsis <dimitris_ha@hotmail.com>
2021-02-05 19:41:37 +0200

    add footer

--- client/src/app/layout/layout.module.ts
+++ client/src/app/layout/layout.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from '../app-routing.module';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [NavbarComponent],
+  declarations: [NavbarComponent, FooterComponent],
   imports: [NgbModule, AppRoutingModule],
-  exports: [NavbarComponent],
+  exports: [NavbarComponent, FooterComponent],
 })
 export class LayoutModule {}
